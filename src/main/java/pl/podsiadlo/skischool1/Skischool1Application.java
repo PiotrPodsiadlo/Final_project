97b9753638a01d9cf3bb183bdcb48af3857bf869
Piotr <piotrekchorzow@interia.eu>
2019-07-15 16:29:23 +0200

    initial

---
+++ src/main/java/pl/podsiadlo/skischool1/Skischool1Application.java
@@ -0,0 +1,15 @@
+package pl.podsiadlo.skischool1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Skischool1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Skischool1Application.class, args);
+        System.out.println("zaczynamy");
+
+    }
+
+}
---
+++ src/main/java/pl/podsiadlo/skischool1/home/HomeController.java
@@ -0,0 +1,14 @@
+package pl.podsiadlo.skischool1.home;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    @ResponseBody
+    public String home() {
+        return "hello world !!!";
+    }
+}
