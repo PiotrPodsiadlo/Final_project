843ddab53d47a7e9db88ebc91312cee1b81a0adc
Piotr <piotrekchorzow@interia.eu>
2019-07-15 18:10:50 +0200

    class Qualification describing instructor's skills

--- src/main/java/pl/podsiadlo/skischool1/function/Function.java
+++ src/main/java/pl/podsiadlo/skischool1/function/Function.java
@@ -9,16 +9,37 @@ public class Function {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String functionType;
+    private String type;
+
+
+    public Function() {
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public Function setId(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getFunctionType() {
+        return type;
+    }
+
+    public Function setFunctionType(String type) {
+        this.type = type;
+        return this;
+    }
 }
 
 
 /**
  *
- * Function definiuje, czy instrutkor jest snowboardzista, czy narciarzem, czy oboma.
- *  relacje Instructor -> Function = ManyToMany
- *  relacje Function -> Qualification = OneToMany
- *
- *
+ *  Function should define if Instructor is a ski or snowboard instructor (or both). Functions MIGHT NOT be defined by user
+ *  relation: Instructor -> Function = ManyToMany
+ *  relation: Function -> Qualification = OneToMany
  *
  */
---
+++ src/main/java/pl/podsiadlo/skischool1/qualification/Qualification.java
@@ -0,0 +1,69 @@
+package pl.podsiadlo.skischool1.qualification;
+
+import pl.podsiadlo.skischool1.function.Function;
+
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+
+
+@Entity
+@Table(name = "qualifications")
+public class Qualification {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    String name;
+    Function function;
+    double hourlySalary;
+
+    public Qualification() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Qualification setId(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Qualification setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public Function getFunction() {
+        return function;
+    }
+
+    public Qualification setFunction(Function function) {
+        this.function = function;
+        return this;
+    }
+
+    public double getHourlySalary() {
+        return hourlySalary;
+    }
+
+    public Qualification setHourlySalary(double hourlySalary) {
+        this.hourlySalary = hourlySalary;
+        return this;
+    }
+}
+/**
+ * Qualification describes level of abilities of instructor. Specific categories are
+ * linked with specific salary. Qualifications can be added by admin .Instructor's  basic hourly salary depends on his/her qualification
+ * relation: Qualification -> Salary = OneToOne
+ * relation: Qualification <- Function OnetoMany
+ * Only instructors who are both snowboard and ski instructors can have two Qualifications, the rest can have only one .
+ *
+ *
+ *
+* */
