a8f77cef62d7bd1ee932c648ac78a5568dd1b26a
Piotr <piotrekchorzow@interia.eu>
2019-07-16 11:32:21 +0200

    DTOs and services removed for development purposes

--- src/main/java/pl/podsiadlo/skischool1/customer/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-//package pl.podsiadlo.skischool1.customer;
-//
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-//import pl.podsiadlo.skischool1.function.Function;
-//
-//@Repository
-//public interface CustomerRepository extends JpaRepository<Customer, Long> {
-//}
